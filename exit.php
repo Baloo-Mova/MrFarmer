18645df52811b48da3b14ad968e8899145d48f1c
DESKTOP-J9LBAEI\petly <petliak.serhii@gmail.com>
2016-11-30 22:40:41 +0200

    Revert "add files"
    This reverts commit e31a561565c7e5b24f016d09b8d092ec0e68629c.

--- exit.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-unset($_SESSION["user"]);
-unset($_SESSION["user_id"]);
-unset($_SESSION["admin"]);
-@session_destroy();
-Header("Location: /");
-?>
-	
--- inc/_set_referer.php
+++
@@ -1,7 +0,0 @@
-<?PHP 
-if(isset($_GET["i"])){
-$_rid = (intval($_GET["i"]) > 0) ? intval($_GET["i"]) : 1; 
-setcookie("i",$_rid,time()+2592000);
-header("Location: /");
-}
-?>
